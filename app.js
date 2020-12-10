6c8f6780df6b8f0d5807aeace14eb650b4b55f65
jarooda <jaluwibowoaji@gmail.com>
2020-12-11 03:27:19 +0700

    Add New Rule 1. betting by Color : jika menang, maka akan mendapatkan uang senilai yang di betting 2. menambah countdown 3. membuat tampilan uang lebih real 4. detik masih 5, akan diubah sebelum production

--- app.js
+++ app.js
@@ -49,36 +49,224 @@ const probability = [
     num: 10,
     color: 'black'
   },
+  {
+    num: 11,
+    color: 'black'
+  },
+  {
+    num: 12,
+    color: 'red'
+  },
+  {
+    num: 13,
+    color: 'black'
+  },
+  {
+    num: 14,
+    color: 'red'
+  },
+  {
+    num: 15,
+    color: 'black'
+  },
+  {
+    num: 16,
+    color: 'red'
+  },
+  {
+    num: 17,
+    color: 'black'
+  },
+  {
+    num: 18,
+    color: 'red'
+  },
+  {
+    num: 19,
+    color: 'red'
+  },
+  {
+    num: 20,
+    color: 'black'
+  },
+  {
+    num: 21,
+    color: 'red'
+  },
+  {
+    num: 22,
+    color: 'black'
+  },
+  {
+    num: 23,
+    color: 'red'
+  },
+  {
+    num: 24,
+    color: 'black'
+  },
+  {
+    num: 25,
+    color: 'red'
+  },
+  {
+    num: 26,
+    color: 'black'
+  },
+  {
+    num: 27,
+    color: 'red'
+  },
+  {
+    num: 28,
+    color: 'black'
+  },
+  {
+    num: 29,
+    color: 'black'
+  },
+  {
+    num: 30,
+    color: 'red'
+  },
+  {
+    num: 31,
+    color: 'black'
+  },
+  {
+    num: 32,
+    color: 'red'
+  },
+  {
+    num: 33,
+    color: 'black'
+  },
+  {
+    num: 34,
+    color: 'red'
+  },
+  {
+    num: 35,
+    color: 'black'
+  },
+  {
+    num: 36,
+    color: 'red'
+  },
 ]
 let totalPrizeMoney = 50000
+let currentresult = {
+  num: 1,
+  color: 'red'
+}
+let seconds = 5
 
+
+// ? socket.io here
 io.on('connect', socket => {
   socket.emit('init', { history, totalPrizeMoney, probability })
-  // ! jadikan 1.000.000
-  socket.on('betting', (payload) => {
-    let message = `${payload.name} bet Rp. ${Number(payload.bet).toLocaleString('id')}`
-    history.push({ message })
-    io.emit('message', { message } )
+
+  // ? play = merandom result
+  socket.on('play', () => {
+    seconds = 5
 
     const index = Math.floor(Math.random() * probability.length)
+    currentresult = probability[index]
 
-    if (probability[index].num != payload.num) {
-      totalPrizeMoney += +payload.bet
-      message = `${payload.name} Kalah, Total Prize Money bertambah menjadi ${ totalPrizeMoney }`
-      history.push({ message })
+    // ? timer
+    const countDown = setInterval(() => {
+      io.emit('result', {result: seconds} )
+      seconds--
+  
+      if (seconds < 0) {
+        clearInterval(countDown)
+        io.emit('result', {result: `${currentresult.num} ${currentresult.color}`} )
+        currentresult = 0
+      }
+    }, 1000)
+  })
 
-      socket.emit('resultWrong', { bet: payload.bet})
-      io.emit('messageWrong', { message, bet: payload.bet } )
+  // ! jadikan 1.000.000
+  socket.on('betting', (payload) => {
+    let message = `${payload.name} betting all his money total Rp. ${Number(payload.bet).toLocaleString('id')}`
+    history.unshift({ message })
+    io.emit('message', { message } )
+
+    if (currentresult.num != payload.num) {
+
+      // ? timer
+      const countDown = setInterval(() => {
+
+        if (seconds < 0) {
+          clearInterval(countDown)
+          totalPrizeMoney += +payload.bet
+          message = `${payload.name} Lose, Total Prize Money now is Rp. ${Number(totalPrizeMoney).toLocaleString('id')}`
+          history.unshift({ message })
+
+          socket.emit('resultWrong', { bet: payload.bet})
+          io.emit('messageWrong', { message, bet: payload.bet } )
+
+        }
+      }, 1000)
+
     } else {
-      message = `${payload.name} Menang Sejumlah Rp. ${totalPrizeMoney},- !!!, Total Prize Money di stock ulang oleh bandar menjadi Rp. 50.000,-`
-      history.push({ message })
-      
-      socket.emit('resultRight', { gain: totalPrizeMoney })
-      io.emit('messageRight', { message } )
-      totalPrizeMoney = 50000
-    }
 
-    io.emit('result', {result: probability[index]} )
+      // ? timer
+      const countDown = setInterval(() => {
+
+        if (seconds < 0) {
+          clearInterval(countDown)
+          message = `${payload.name} Win and got Rp. ${Number(totalPrizeMoney).toLocaleString('id')},- !!!, Total Prize Money Restocked to Rp. 50.000,-`
+          history.unshift({ message })
+          
+          socket.emit('resultRight', { gain: totalPrizeMoney })
+          io.emit('messageRight', { message } )
+          totalPrizeMoney = 50000
+
+        }
+      }, 1000)
+      
+    }
+  })
+
+  socket.on('bettingByColor', (payload) => {
+    let message = `${payload.name} bet Rp. ${Number(payload.bet).toLocaleString('id')}`
+    history.unshift({ message })
+    io.emit('message', { message } )
+
+    if (currentresult.color != payload.color) {
+
+      // ? timer
+      const countDown = setInterval(() => {
+
+        if (seconds < 0) {
+          clearInterval(countDown)
+          totalPrizeMoney += +payload.bet
+          message = `${payload.name} Lose, Total Prize Money now is Rp. ${Number(totalPrizeMoney).toLocaleString('id')}`
+          history.unshift({ message })
+
+          socket.emit('resultWrong', { bet: payload.bet})
+          io.emit('messageWrong', { message, bet: payload.bet } )
+
+        }
+      }, 1000)
+
+    } else {
+
+      // ? timer
+      const countDown = setInterval(() => {
+
+        if (seconds < 0) {
+          clearInterval(countDown)
+          message = `${payload.name} Win and got Rp. ${Number(payload.bet).toLocaleString('id')},- !!!`
+          history.unshift({ message })
+          
+          socket.emit('resultRightColor', { gain: payload.bet })
+          io.emit('messageRight', { message } )
+        }
+      }, 1000)
+      
+    }
   })
 });
 
